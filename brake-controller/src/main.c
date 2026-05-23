6ec5ca28b14c4b30f0d7d950265aceaabb28653d
Jack Mills <s4698717@student.uq.edu.au>
2026-05-23 15:24:29 +1000

    MQTT connection code (not tested on board).
    Requirements to for testing:
    
    * PC has ip address 172.20.10.10. My PC has been set up to only use this IP address.
    
    * mosquitto broker is set up. This can be done using the mosquitto command:
    ```
    $ cat > /tmp/mqtt.conf <<'EOF'
    listener 1883 0.0.0.0
    allow_anonymous true
    EOF
    $ sudo systemctl stop mosquitto
    $ mosquitto -c /tmp/mqtt.conf -v
    ```
    
    * you can also receive and send MQTT messages
    ```
    $ mosquitto_sub -h 172.20.10.10 -t '#' -v #receives
    $ mosquitto_pub -h 172.20.10.3 -t zephyr_sample/sensor -m 'hello' #sends
    ```

--- brake-controller/src/main.c
+++ brake-controller/src/main.c
@@ -1,13 +1,23 @@
 /* source: https://hub.mender.io/t/connectivity-with-zephyr-part-1-wifi-on-esp32-s3/8130 */
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
-#include <zephyr/net/wifi_mgmt.h>
-#include <zephyr/net/net_if.h>
-#include <zephyr/net/net_event.h>
+#include <zephyr/net/mqtt.h>
 
 #include "wifi.h"
+#include "mqtt_client.h"
 
 LOG_MODULE_REGISTER(wifi_app, LOG_LEVEL_INF);
+
+/* MQTT client struct */
+static struct mqtt_client client_ctx;
+
+/* MQTT publish work item */
+//struct k_work_delayable mqtt_publish_work;
+
+//static struct net_mgmt_event_callback net_l4_mgmt_cb;
+
+/* Network connection semaphore */
+//K_SEM_DEFINE(net_conn_sem, 0, 1);
 
 int main(void)
 {
@@ -21,19 +31,33 @@ int main(void)
     printk("Connecting...\n");
     error = wifi_connect();
     if (error) {
-        printk("Failed to connect\n");
-        return 0;
+        printk("Failed to connect [error: %i]\n", error);
+        return error;
     }
 
     printk("Get IP address\n");
     error = wifi_wait_for_ip_addr();
     if (error) {
-        printk("IP address wait timed out\n");
-        return 0;
+        printk("IP address wait timed out [error: %i]\n", error);
+        return error;
     }
 
-    printk("Disconnecting\n");
-    (void) wifi_disconnect();
+    printk("Initialising MQTT\n");
+	error = app_mqtt_init(&client_ctx);
+	if (error) {
+        printk("MQTT failed to initialise [error: %i]\n", error);
+        return error;
+	}
+
+    printk("Connecting to broker\n");
+	app_mqtt_connect(&client_ctx); /* block until MQTT connection is up */
+
+    printk("Connected!\n");
+
+    while (1) {
+        k_sleep(K_MSEC(1000));
+        printk("God I love busy waiting\n");
+    }
 
     return 0;
 }
